df8493ad90920787c27e90f01cb4911f6a9f5782
MarwaElhelali <32886011+MarwaElhelali@users.noreply.github.com>
2018-03-26 21:16:40 +0200

    m

---
+++ android/Gamety/app/src/main/java/com/mahmoudreda/gamety/ListItem.java
@@ -0,0 +1,13 @@
+package com.mahmoudreda.gamety;
+
+/**
+ * Created by marwa elhelali on 26-Mar-18.
+ */
+
+public class ListItem {
+
+    public String name ;
+    public String link ;
+
+    ListItem(String name , String link) { this.name=name ;  this.link=link;}
+}
