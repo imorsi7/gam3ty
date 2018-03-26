7f7238a06bc60b6ae058f11837ae6170cba56095
Mahmoud reda <27293458+Mahmoudreda96@users.noreply.github.com>
2018-03-27 00:25:48 +0200

    sign up

--- android/Gamety/app/src/main/java/com/mahmoudreda/gamety/Sign_up.java
+++ android/Gamety/app/src/main/java/com/mahmoudreda/gamety/Sign_up.java
@@ -13,16 +13,29 @@ public class Sign_up extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
-        EditText x = findViewById(R.id.editText_name_sign_up);
-        EditText y = findViewById(R.id.editText_id_sign_up);
-        EditText z = findViewById(R.id.editText_email_sign_up);
-        EditText g = findViewById(R.id.editText_password_sign_up);
-        EditText h = findViewById(R.id.editText_confirm_password);
+        final EditText x = findViewById(R.id.editText_first_name_sign_up);
+        final EditText y = findViewById(R.id.editText_middle_name_sign_up);
+        final EditText p = findViewById(R.id.editText_last_name_sign_up);
+        final EditText w = findViewById(R.id.editText_id_sign_up);
+        final EditText z = findViewById(R.id.editText_email_sign_up);
+        final EditText g = findViewById(R.id.editText_password_sign_up);
+
         Button l = findViewById(R.id.button_sign_up_data);
         l.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(getApplicationContext(), schedule.class);
+                final String first_name = x.getText().toString();
+                final String second_name = y.getText().toString();
+                final String last_name = p.getText().toString();
+                final String id_sign_up = w.getText().toString();
+                final String email_sign_up = z.getText().toString();
+                final String password_sign_up = g.getText().toString();
+
+
+                sign_up_to_data_base my_data=new sign_up_to_data_base();
+                my_data.x=getApplicationContext();
+                my_data.execute(first_name,second_name,last_name,id_sign_up,email_sign_up,password_sign_up);
+                Intent i = new Intent(getApplicationContext(), sign_in.class);
                 startActivity(i);
             }
         });
