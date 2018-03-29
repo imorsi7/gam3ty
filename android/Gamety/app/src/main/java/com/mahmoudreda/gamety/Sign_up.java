f637d6e86af42f4c3aa4c680260a7bfa9102c977
Mahmoud reda <27293458+Mahmoudreda96@users.noreply.github.com>
2018-03-29 17:03:08 +0200

    ok

--- android/Gamety/app/src/main/java/com/mahmoudreda/gamety/Sign_up.java
+++ android/Gamety/app/src/main/java/com/mahmoudreda/gamety/Sign_up.java
@@ -14,14 +14,14 @@ public class Sign_up extends Activity {
 
     EditText x, y, p, w, z, g;
     Button l;
-    String first_name, second_name, last_name, id_sign_up, email_sign_up, password_sign_up;
+    String id_sign_up, first_name, middle_name, last_name, email_sign_up, password_sign_up;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        p = findViewById(R.id.editText_last_name_sign_up);
         x = findViewById(R.id.editText_first_name_sign_up);
         y = findViewById(R.id.editText_middle_name_sign_up);
-        p = findViewById(R.id.editText_last_name_sign_up);
         w = findViewById(R.id.editText_id_sign_up);
         z = findViewById(R.id.editText_email_sign_up);
         g = findViewById(R.id.editText_password_sign_up);
@@ -31,10 +31,10 @@ public class Sign_up extends Activity {
         l.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                first_name = x.getText().toString();
-                second_name = y.getText().toString();
-                last_name = p.getText().toString();
                 id_sign_up = w.getText().toString();
+                first_name = x.getText().toString();
+                middle_name = y.getText().toString();
+                last_name = p.getText().toString();
                 email_sign_up = z.getText().toString();
                 password_sign_up = g.getText().toString();
 
@@ -42,18 +42,18 @@ public class Sign_up extends Activity {
                 SharedPreferences.Editor editor = sharedpreferences.edit();
                 editor.putString("Name", id_sign_up);
                 editor.commit();
-                if (first_name.length() >= 1 || second_name.length() >= 1 || last_name.length() >= 1 || id_sign_up.length() >= 1 || email_sign_up.length() >= 1 || password_sign_up.length() >= 1) {
-                    if (id_sign_up.length() == 9) {
+                if (first_name.length() >= 1 || middle_name.length() >= 1 || last_name.length() >= 1 || id_sign_up.length() >= 1 || email_sign_up.length() >= 1 || password_sign_up.length() >= 1) {
+                    if (id_sign_up.length() == 8) {
                         sign_up_to_data_base my_data = new sign_up_to_data_base();
                         my_data.x = getApplicationContext();
-                        my_data.execute(first_name, second_name, last_name, id_sign_up, email_sign_up, password_sign_up);
+                        my_data.execute(id_sign_up,first_name, middle_name, last_name,  email_sign_up, password_sign_up);
                         Toast.makeText(Sign_up.this, "welcome student", Toast.LENGTH_LONG).show();
                         Intent i = new Intent(getApplicationContext(), sign_in.class);
                         startActivity(i);
                     } else {
                         sign_up_to_data_base my_data = new sign_up_to_data_base();
                         my_data.x = getApplicationContext();
-                        my_data.execute(first_name, second_name, last_name, id_sign_up, email_sign_up, password_sign_up);
+                        my_data.execute( id_sign_up,first_name, middle_name, last_name, email_sign_up, password_sign_up);
                         Intent i = new Intent(getApplicationContext(), schedule.class);
                         startActivity(i);
                     }
